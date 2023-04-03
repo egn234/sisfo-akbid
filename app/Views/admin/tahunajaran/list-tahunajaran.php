8811ca702e2ca6136b98267cbe93b8333607a029
Rifqi Ryandi <40005-c02hg3huq05d@40005-C02HG3HUQ05Ds-MacBook-Pro.local>
2023-04-03 09:58:44 +0700

    change all footer

--- app/Views/admin/tahunajaran/list-tahunajaran.php
+++ app/Views/admin/tahunajaran/list-tahunajaran.php
@@ -40,18 +40,8 @@
                     </div>
                 </div>
             </main>
-            <footer class="py-4 bg-light mt-auto">
-                <div class="container-fluid px-4">
-                    <div class="d-flex align-items-center justify-content-between small">
-                        <div class="text-muted">Copyright &copy; Your Website 2022</div>
-                        <div>
-                            <a href="#">Privacy Policy</a>
-                            &middot;
-                            <a href="#">Terms &amp; Conditions</a>
-                        </div>
-                    </div>
-                </div>
-            </footer>
+            <?= $this->include('partials/footer') ?>
+
         </div>
     </div>
 
