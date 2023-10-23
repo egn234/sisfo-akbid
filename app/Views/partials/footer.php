a05461247d091a8cb47dd8d2cf896ee0bc21306c
Egan Kusmaya <egn234@gmail.com>
2023-10-23 13:44:52 +0700

    new fixes

--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -1,7 +1,7 @@
 <footer class="py-4 bg-light mt-auto">
     <div class="container-fluid px-4">
         <div class="d-flex align-items-center justify-content-between small">
-            <div class="text-muted">Copyright &copy; Akademi Kebidanan Annisa Jaya <img src="<?=base_url('')?>/img/ikbis_logo_t.png" width="25" alt=""></div>
+            <div class="text-muted">Copyright &copy; Institut Kesehatan Dan Bisnis Annisa <img src="<?=base_url('')?>/img/ikbis_logo_t.png" width="25" alt=""></div>
             <!-- <div>
                 <a href="#">Privacy Policy</a>
                 &middot;
